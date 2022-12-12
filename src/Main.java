34804ed3e1f150d514a37a7bc7674ac308452410
roman <roman-1901@mail.ru>
2022-12-12 20:55:11 +0300

    Humans

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
     public static void main(String[] args) {
+        Human max = new Human(1988, "Максим", "Минск", "Бренд-менеджер");
+        Human annie = new Human(1993, "Анна", "Москва", "Методист образовательных программ");
+        Human kate = new Human(1992, "Катя", "Калининград", "Продакт-менеджер");
+        Human artem = new Human(1995, "Артем", "Москва", "Директор по развитию бизнеса");
 
-        System.out.println("Hello world!");
+        max.InformationOfHuman();
+        annie.InformationOfHuman();
+        kate.InformationOfHuman();
+        artem.InformationOfHuman();
     }
 }
