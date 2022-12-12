fc4f0f4dc9dc6952d59023fb9b6fa2c12c7c7e31
roman <roman-1901@mail.ru>
2022-12-12 22:42:40 +0300

    Humans

--- src/Human.java
+++ src/Human.java
@@ -5,7 +5,11 @@ public class Human {
     String jobTitle;
 
     public Human(int yearOfBirth, String name, String town, String jobTitle) {
-        this.yearOfBirth = yearOfBirth;
+        if (yearOfBirth >= 0) {
+            this.yearOfBirth = yearOfBirth;
+        } else {
+            this.yearOfBirth = Math.abs(yearOfBirth);
+        }
         this.name = name;
         this.town = town;
         this.jobTitle = jobTitle;
@@ -13,6 +17,10 @@ public class Human {
 
     @Override
     public String toString() {
-        return "Привет! Меня зовут " + name + ". Я из города " + town + ". Я родился в " + yearOfBirth + " году. Я работаю на должности "+ jobTitle +". Будем знакомы!";
+        if (name == null || name.isEmpty() || town == null || town.isEmpty() || jobTitle == null || jobTitle.isEmpty()) {
+            return "Информация не указана";
+        } else {
+            return "Привет! Меня зовут " + name + ". Я из города " + town + ". Я родился в " + yearOfBirth + " году. Я работаю на должности "+ jobTitle +". Будем знакомы!";
+        }
     }
 }
